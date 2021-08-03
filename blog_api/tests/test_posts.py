c4df5ef193ff3eabdd15fdc62a30275e7482c636
DenMaslov <20denismaslov@gmail.com>
2021-08-03 14:32:18 +0300

    Improve 2

--- blog_api/tests/test_posts.py
+++ blog_api/tests/test_posts.py
@@ -4,51 +4,67 @@ from main import app
 
 client = TestClient(app)
 
-valid_data = {
-  "userId": 1,
-  "title": "string",
-  "body": "string"
-}
+@pytest.fixture
+def valid_data():
+    valid_d = {
+        "userId": 1,
+        "title": "string",
+        "body": "string"
+    }
+    return valid_d
 
-invalid_data = {
-  "userId": "sds",
-  "title": 12,
-  "body": "string"
-}
+@pytest.fixture
+def invalid_data():
+    data = {
+    "userId": "sds",
+    "title": 12,
+    "body": "string"
+    }
+    return data
 
+@pytest.fixture
+def post():
+    post = {
+        "id": 1,
+        "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+        "author": {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "phone": "1-770-736-8031 x56442"
+        }
+    }
+    return post
 
-def test_post_list():
+def test_post_list(post):
     response = client.get("/posts")
     assert response.status_code == 200
     resp = response.json()[0]
-    assert resp["author"]["id"]
-    assert resp["author"]["name"]
-    assert resp["author"]["phone"]
-    assert resp["author"]["email"]
-    assert resp["author"]["username"]
+    assert resp["id"] == post['id']
+    assert resp["author"] == post['author']
+    assert resp["title"] == post['title']
 
-def test_create_post():
+def test_create_post(valid_data):
     response = client.post("/posts/", json=valid_data)
     assert response.status_code == 201
     resp = response.json()
-    assert resp["author"]["id"]
-    assert resp["author"]["name"]
-    assert resp["author"]["phone"]
-    assert resp["author"]["email"]
-    assert resp["author"]["username"]
+    assert resp["title"] == valid_data['title']
 
-def test_invalid_creation_post():
+def test_invalid_creation_post(invalid_data):
     response = client.post("/posts/", json=invalid_data)
     assert response.status_code == 422
     resp = response.json()
     assert "author" not in resp
 
-def test_get_detail_post():
-    response = client.get("/posts/3")
+def test_get_detail_post(post):
+    response = client.get("/posts/1")
     assert response.status_code == 200
     resp = response.json()
-    assert resp["author"]["id"]
-    assert resp["comments"]
+    assert resp["id"] == post['id']
+    assert resp["author"] == post['author']
+    assert resp["title"] == post['title']
 
 def test_update_post():
     data = {"title": "string", "body": "string"}
